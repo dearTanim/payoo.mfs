a77b1d2593816c1c5e7a6e55f48ce013cab8fe4f
Md Asraful Islam <deartanim@gmail.com>
2024-09-19 19:56:43 +0600

    added a section

---
+++ js/home.js
@@ -0,0 +1,12 @@
+
+document.getElementById('add-money-btn').addEventListener('click', function(event){
+    event.preventDefault();
+
+    // const addAccountInput = document.getElementById('add-account-input').value;
+    // console.log(addAccountInput);
+
+    const addAmountInput = document.getElementById('add-amount-input').value;
+    console.log(addAmountInput);
+
+
+})
---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+document.getElementById('login-btn').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const phoneNumber = document.getElementById('number-input').value;
+
+    const pinNumber = document.getElementById('pin-input').value;
+
+    console.log(phoneNumber, pinNumber);
+
+    if ( phoneNumber === '5' && pinNumber === '1234'){
+        window.location.href = './home.html';
+
+    }
+    else{
+        alert('Incorrect data');
+    }
+
+})
